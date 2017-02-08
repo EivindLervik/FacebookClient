8565c67c9ac433f4f8c9dbef961e43b10b415f4a
Michelsen93 <om.michelsen@gmail.com>
2017-02-08 15:04:05 +0100

    Added som base code

--- app/src/main/java/no/hvl/dat153/facebookclient/GraphApi.java
+++ app/src/main/java/no/hvl/dat153/facebookclient/GraphApi.java
@@ -2,7 +2,15 @@ package no.hvl.dat153.facebookclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class GraphApi extends AppCompatActivity {
 
@@ -20,7 +28,46 @@ public class GraphApi extends AppCompatActivity {
     public void showInformation(View view){
         //TODO - Make a request. Display results
     }
-    public void requestHandler(){
+    public void requestHandlerLikes(){
+        GraphRequest request = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted (
+                            JSONObject object,
+                            GraphResponse response) {
+                        try {
+                            JSONArray likes = object.getJSONArray("data");
+                        }
+                        catch (Exception e){
 
+                        }
+
+                        //TODO - add items to view
+                        //likes{data[{name},{name}]}
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "likes");
+        request.setParameters(parameters);
+        request.executeAsync();
     }
+    public void requestHandlerInformation(){
+        GraphRequest request = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(
+                            JSONObject object,
+                            GraphResponse response) {
+
+                        //TODO - add items to view
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,link");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
 }
