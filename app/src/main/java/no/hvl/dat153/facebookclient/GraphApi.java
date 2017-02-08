a608a4ecd6301969c665435783eac26cd908b98d
Michelsen93 <om.michelsen@gmail.com>
2017-02-08 13:36:23 +0100

    Added GraphApi class with view

---
+++ app/src/main/java/no/hvl/dat153/facebookclient/GraphApi.java
@@ -0,0 +1,15 @@
+package no.hvl.dat153.facebookclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GraphApi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph_api);
+        //TODO - Make a request. Display results
+
+    }
+}
