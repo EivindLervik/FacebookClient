675dbc58dbe2adc90e4d9b0c749650790effe13b
Michelsen93 <om.michelsen@gmail.com>
2017-02-08 13:48:47 +0100

    Added components to view

--- app/src/main/java/no/hvl/dat153/facebookclient/GraphApi.java
+++ app/src/main/java/no/hvl/dat153/facebookclient/GraphApi.java
@@ -2,6 +2,7 @@ package no.hvl.dat153.facebookclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GraphApi extends AppCompatActivity {
 
@@ -10,6 +11,13 @@ public class GraphApi extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph_api);
         //TODO - Make a request. Display results
+
+    }
+
+    public void showLikes(View view){
+
+    }
+    public void showInformation(View view){
 
     }
 }
