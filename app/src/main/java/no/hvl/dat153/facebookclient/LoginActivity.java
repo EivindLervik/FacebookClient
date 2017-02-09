a5f6d092e92b9ebddf3243f00ad5a0ebc0fa89b4
Ragnhild Oppedal <ragnhild.oppedal@vimond.com>
2017-02-09 10:19:34 +0100

    created activity_login.xml and updated LoginActivity.java

--- app/src/main/java/no/hvl/dat153/facebookclient/LoginActivity.java
+++ app/src/main/java/no/hvl/dat153/facebookclient/LoginActivity.java
@@ -1,7 +1,18 @@
 package no.hvl.dat153.facebookclient;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.facebook.login.widget.LoginButton;
+
 public class LoginActivity extends AppCompatActivity {
+
+    LoginButton login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
 }
