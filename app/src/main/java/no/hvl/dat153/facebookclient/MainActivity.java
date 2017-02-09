b44cc9f08732c692b637abebafaf8a5ab8958110
Ragnhild Oppedal <ragnhild.oppedal@vimond.com>
2017-02-09 11:39:45 +0100

    added logut-logic to MainActivity.java

--- app/src/main/java/no/hvl/dat153/facebookclient/MainActivity.java
+++ app/src/main/java/no/hvl/dat153/facebookclient/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.facebook.login.LoginManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logout = (Button)findViewById(R.id.login_button);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logOut();
+                Intent login = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(login);
+                finish();
+            }
+        });
     }
 
     public void graphClick(View view){
