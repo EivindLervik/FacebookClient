3275fff44b906db300076289ed9a3a2dcc214a86
Michelsen93 <om.michelsen@gmail.com>
2017-02-09 10:08:02 +0100

    Added method to hopefully retreive likes in listview

--- app/src/main/java/no/hvl/dat153/facebookclient/GraphApi.java
+++ app/src/main/java/no/hvl/dat153/facebookclient/GraphApi.java
@@ -4,13 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class GraphApi extends AppCompatActivity {
 
@@ -38,13 +45,12 @@ public class GraphApi extends AppCompatActivity {
                             GraphResponse response) {
                         try {
                             JSONArray likes = object.getJSONArray("data");
+                            updateListCallback(likes);
                         }
                         catch (Exception e){
-
+                            Toast.makeText(GraphApi.this, "Could not find likes", Toast.LENGTH_LONG).show();
                         }
 
-                        //TODO - add items to view
-                        //likes{data[{name},{name}]}
                     }
                 });
         Bundle parameters = new Bundle();
@@ -68,6 +74,20 @@ public class GraphApi extends AppCompatActivity {
         parameters.putString("fields", "id,name,link");
         request.setParameters(parameters);
         request.executeAsync();
+    }
+
+    public void updateListCallback(JSONArray likes){
+        ListView lw = (ListView) findViewById(R.id.item_list);
+        ArrayList<String> likesList = new ArrayList<String>();
+        try {
+            for (int i = 0; i < likes.length(); i++) {
+                likesList.add(likes.getJSONObject(i).getString("name"));
+            }
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, likesList);
     }
 
 }
