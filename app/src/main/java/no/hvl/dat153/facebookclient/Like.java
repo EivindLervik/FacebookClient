94b56a4b449eb47beed1a4bffb14a078f9485247
EivindLervik <eivind_fantaboy@msn.com>
2017-02-14 11:11:47 +0100

    Added like-button for Hib

--- app/src/main/java/no/hvl/dat153/facebookclient/Like.java
+++ app/src/main/java/no/hvl/dat153/facebookclient/Like.java
@@ -3,11 +3,19 @@ package no.hvl.dat153.facebookclient;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.share.widget.LikeView;
+
 public class Like extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_like);
+
+        // Add values to the like-view
+        LikeView likeView = (LikeView) findViewById(R.id.like_view);
+        likeView.setObjectIdAndType(
+                "https://www.facebook.com/HiBinfo/?fref=ts",
+                LikeView.ObjectType.PAGE);
     }
 }
