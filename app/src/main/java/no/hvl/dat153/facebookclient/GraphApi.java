04ceb55fa39b4167489d0a643332ba723acd9267
Michelsen93 <om.michelsen@gmail.com>
2017-02-14 13:21:29 +0100

    Finnished

--- app/src/main/java/no/hvl/dat153/facebookclient/GraphApi.java
+++ app/src/main/java/no/hvl/dat153/facebookclient/GraphApi.java
@@ -1,5 +1,6 @@
 package no.hvl.dat153.facebookclient;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,11 +12,14 @@ import android.widget.Toast;
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.login.widget.ProfilePictureView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -52,7 +56,7 @@ public class GraphApi extends AppCompatActivity {
                     }
                 });
         Bundle parameters = new Bundle();
-        parameters.putString("fields", "friends, name, id, gender");
+        parameters.putString("fields", "friends, name, id, gender, picture");
         request.setParameters(parameters);
         request.executeAsync();
     }
@@ -67,6 +71,9 @@ public class GraphApi extends AppCompatActivity {
             String totalFriends = "friends: " + object.getJSONObject("friends").getJSONObject("summary").getString("total_count");
             String name = "name: " + object.getString("name");
             String gender = "gender: " + object.getString("gender");
+            ProfilePictureView profilePicture = (ProfilePictureView) findViewById(R.id.profilePicView);
+            String profileId = object.getString("id");
+            profilePicture.setProfileId(profileId);
             arrayList.add(totalFriends);
             arrayList.add(name);
             arrayList.add(gender);
@@ -77,6 +84,16 @@ public class GraphApi extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
         lw.setAdapter(adapter);
+    }
+
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+        } catch (Exception e) {
+            return null;
+        }
     }
 
 }
