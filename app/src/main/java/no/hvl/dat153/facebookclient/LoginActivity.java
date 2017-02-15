fcbbe20fa6df5e80195037760df3f4673ebe42e8
Ragnhild Oppedal <ragnhild.oppedal@vimond.com>
2017-02-15 13:19:56 +0100

    relocated loginbutton to the bottom in login and main

--- app/src/main/java/no/hvl/dat153/facebookclient/LoginActivity.java
+++ app/src/main/java/no/hvl/dat153/facebookclient/LoginActivity.java
@@ -6,13 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
-import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.Profile;
-import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
