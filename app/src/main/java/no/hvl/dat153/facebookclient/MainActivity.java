4a960cb4800c979d9791e08c3f59d7674a66a06e
Ragnhild Oppedal <ragnhild.oppedal@vimond.com>
2017-02-16 09:07:40 +0100

    updated MainActivity.java

--- app/src/main/java/no/hvl/dat153/facebookclient/MainActivity.java
+++ app/src/main/java/no/hvl/dat153/facebookclient/MainActivity.java
@@ -45,13 +45,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void shareClick(View view){
-        ShareLinkContent content = new ShareLinkContent.Builder()
-                .setContentTitle("This app is great!")
-                .setContentDescription(
-                        "I just used this app that lets you see your facebook profile. Learn about facebook API today!")
-                .setContentUrl(Uri.parse("http://developers.facebook.com/android"))
-                .build();
-        shareDialog.show(content);
+        Intent intent = new Intent(this, ShareThis.class);
+        startActivity(intent);
 
     }
 
